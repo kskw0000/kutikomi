f01e1b09638c85836a947a34f548564a4e6395a0
kskw0000 <kskw2525@gmail.com>
2023-07-07 11:20:07 +0900

    php version３

--- .history/app/Models/Qual_User_20230707111126.php
+++ .history/app/Models/Qual_User_20230707111126.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Models; // 修正した部分
+namespace App\Models\Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Qual_User extends Model
+class User extends Model // この行を変更します。
 {
     use HasFactory;
     protected $table = 'tbl_qual_list';
--- .history/app/Models/model/Qual_User_20230707104924.php
+++ .history/app/Models/model/Qual_User_20230707104924.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models; // 修正した部分
+namespace App\Models\Model; // この行を変更します。
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
